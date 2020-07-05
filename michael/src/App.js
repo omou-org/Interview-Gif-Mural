b4a3001adc9979167f344692fe19b139a3c27f37
Michael Li <ml5885@pleasantonusd.net>
2020-07-04 17:15:12 -0700

    Created GifCard component

--- michael/src/App.js
+++ michael/src/App.js
@@ -1,17 +1,48 @@
-// eslint-disable-next-line
-import React, { Component } from 'react';
-// eslint-disable-next-line
-import PropTypes from 'prop-types';
 import gifList from './gifList';
+import "./App.css";
+import React, { useEffect } from "react";
+import * as fetchActions from "./actions/gifActions";
+import { useDispatch, useSelector } from "react-redux";
+
+//Material UI imports
+import GifCard from "./GifCard";
+import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar"
+import Typography from "@material-ui/core/Typography"
+import { makeStyles } from "@material-ui/core";
 
 function App() {
+  let words = [
+    "idea",
+    "animal",
+    "quaint",
+    "football",
+    "concentration",
+    "witness",
+    "slap",
+    "apparatus",
+    "north",
+    "nominate"
+  ];
+
+
+  const gridCard = words.map(card => {
+    return (
+      <Grid key={card} item sm={6} align="center">
+        <GifCard key={card} name={card} link={card.link} />
+      </Grid>
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        Test 
-      </header>
       <div>
-        <gifList />
+        <Grid
+          container
+          spacing={2}
+        >
+          {gridCard}
+        </Grid>
       </div>
     </div>
   );
